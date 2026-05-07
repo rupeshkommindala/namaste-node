688e5ff075b95e5f554427ef80305ceba0395f0c
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-07 13:10:51 +0530

    module.export and require

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const { calculateSum, calculateMultiply } = require("./calculate/index");
 
+const data = require("./data.json");
+
 var name = "Namaste NodeJS";
 
 var a = 10;
 var b = 20;
+
+console.log(data);
 
 calculateSum(a, b);
 calculateMultiply(a, b);
